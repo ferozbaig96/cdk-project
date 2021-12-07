764e4627be69a151a61f9b362d2556ce6bb36ad0
Mohd Feroz Baig <baigmohd@amazon.com>
2021-12-07 15:56:23 +0530

    basic codecommit and codepipeline

--- src/main/java/com/myorg/CdkProjectStack.java
+++ src/main/java/com/myorg/CdkProjectStack.java
@@ -2,7 +2,13 @@ package com.myorg;
 
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.pipelines.CodeBuildStep;
+import software.amazon.awscdk.pipelines.CodePipeline;
+import software.amazon.awscdk.pipelines.CodePipelineSource;
+import software.amazon.awscdk.services.codecommit.Repository;
 import software.constructs.Construct;
+
+import java.util.Arrays;
 
 public class CdkProjectStack extends Stack {
 	public CdkProjectStack(final Construct scope, final String id) {
@@ -11,6 +17,26 @@ public class CdkProjectStack extends Stack {
 
 	public CdkProjectStack(final Construct scope, final String id, final StackProps props) {
 		super(scope, id, props);
+
+		// CodeCommit repository
+		final Repository repository = Repository.Builder.create(this, Constants.PROJECT_NAME + "Repository")
+			.repositoryName(Constants.PROJECT_NAME + "Repository")
+			.build();
+
+		// CodePipeline
+		final CodePipeline codePipeline = CodePipeline.Builder.create(this, Constants.PROJECT_NAME + "Pipeline")
+			.pipelineName(Constants.PROJECT_NAME + "Pipeline")
+			.synth(CodeBuildStep.Builder.create("DeployToLambdaVersion")
+				.input(CodePipelineSource.codeCommit(repository, Constants.VCS_REPOSITORY_BRANCH))
+				.commands(Arrays.asList(
+					"echo ls",
+					"ls",
+					"echo pwd",
+					"pwd"
+				))
+				.build()
+			).build();
+
 
 	}
 }
--- src/main/java/com/myorg/Constants.java
+++ src/main/java/com/myorg/Constants.java
@@ -2,4 +2,5 @@ package com.myorg;
 
 public class Constants {
 	public static final String PROJECT_NAME = "CdkProject";
+	public static final String VCS_REPOSITORY_BRANCH = "main";
 }
